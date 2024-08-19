1a21ff140be3c3aee2cb90efa78568a07464fdad
ChrisRodVal <chrisrod123dsad@gmail.com>
2024-08-19 10:46:06 -0700

    PredictionBuysPython

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template, url_for
+import joblib
+import sklearn
+
+
+app= Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('ingresarDatos.html')  #   pagina principal
+
+
+@app.route("/predecir", methods=["POST",])
+def predecir():
+    inicio = int(request.form['inicio'])
+    cursos = int(request.form['cursos'])
+    contacto = int(request.form['contacto'])
+
+    modelo=joblib.load('C:/Users/HP/Desktop/InteligenciaArtificialCRV/PrediccionCompra/modelos/modelo_guardado.pkl')
+    #prediccion=modelo.predict([[1,0,1]])
+    prediccion=modelo.predict([[inicio,cursos,contacto]])
+    if prediccion == 0:
+        resultado="No Compra"
+        foto="noComprar.jpg"
+    else:
+        resultado="Compra"
+        foto="comprar.jpg"
+
+    return render_template('resultado.html', resultado=resultado, foto=foto)  #   pagina resultado
